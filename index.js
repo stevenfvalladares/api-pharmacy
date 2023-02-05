76e16951538c40c109792dd25acfa2de28015128
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-05 12:29:54 -0300

    The main file index.js is updated

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express()
-app.listen(3000, console.log('Server ON'))
+const express = require("express");
+const app = express();
+app.listen(3000, console.log("Server running from port 3000"));
 
-const {  } = require('./consultas')
+const { getMedicines, getStaff } = require("./consultas");
 
-app.get('/medicamentos', async (req, res) => {
-  res.json()
-})
+app.get("/medicines", async (req, res) => {
+  const queryStrings = req.query;
+  const medicines = await getMedicines(queryStrings);
+  res.json(medicines);
+});
 
+app.get("/staff", async (req, res) => {
+  const queryStrings = req.query;
+  const staff = await getStaff(queryStrings);
+  res.json(staff);
+});
