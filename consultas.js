0506cda78023d9e5291f45e941939cd265fae124
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-05 12:29:13 -0300

    The consultas.js file is updated

--- consultas.js
+++ consultas.js
@@ -1,13 +1,26 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    password: "postgres",
-    database: "farmacia",
-    port: 5432,
-    allowExitOnIdle: true
+  user: "steven",
+  host: "localhost",
+  password: "22038367",
+  database: "pharmacy",
+  port: 5432,
+  allowExitOnIdle: true,
 });
 
+const getMedicines = async ({ limits = 10 }) => {
+  const query = "SELECT * FROM medicines LIMIT $1";
+  const values = [limits];
+  const { rows: medicines } = await pool.query(query, values);
+  return medicines;
+};
 
-module.exports = {}
+const getStaff = async ({ limits = 10 }) => {
+  const query = "SELECT * FROM staff LIMIT $1";
+  const values = [limits];
+  const { rows: staff } = await pool.query(query, values);
+  return staff;
+};
+
+module.exports = { getMedicines, getStaff };
